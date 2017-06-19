de0b4840012a945bbae2c42e160ffadeb270c7de
Andrew Vitacek <avitacek@gmail.com>
2017-06-19 16:48:10 -0500

    adding adjustments to the ui example

--- assets/tempotrip/js/dist/tempotrip.globals.bundle.js
+++ assets/tempotrip/js/dist/tempotrip.globals.bundle.js
@@ -54,7 +54,15 @@ var hasTouch = 'ontouchstart' in window;
 var START_EV = hasTouch ? 'touchstart' : 'mousedown', 
         // but....it's the best solution I have
     CANCEL_EV = hasTouch ? 'touchcancel' : 'touchcancel';
-  
+
+(function( $ ) {
+	$(document).ready(function(){
+	    $(".addBtn").click(function(){
+	        $("<div class='row travel-points-row'><div class='col-sm-5'><select id='airline_carriers'><option value='?????''>Alaska Airlines – Mileage Plan</option><option value='?????'>American Airlines – AAdvantage</option><option value='?????''>Delta Air Lines – SkyMiles</option><option value='?????''>Frontier Airlines – EarlyReturns</option><option value='?????''>Hawaiian Airlines – HawaiianMiles</option><option value='?????'>JetBlue Airways – TrueBlue</option><option value='?????'>Southwest Airlines – Rapid Rewards</option><option value='?????'>Spirit Airlines – FREE SPIRIT</option><option value='?????'>United Airlines – MileagePlus</option><option value='?????'>Virgin America – eleVAte</option></select></div><div class='col-sm-5'><input type='text' name='airlinePoints' class='airline-points' autocomplete='off' placeholder='XXXXXXXXXXXXX'></div><div class='col-sm-2'><div class='removeBtn'></div></div></div>").appendTo('.points');
+	    }); 
+	});
+})(jQuery);
+
 
 //Global JS File//
 
